b7fe72c05efa57a0e1cca1d095fbda2d3d865372
Helloootiger <131229715+Helloootiger@users.noreply.github.com>
2025-11-30 09:44:06 +0000

    Added Day-3 excercises - learnt to pass argument variable and environmment variable

---
+++ Day-3/env_var.py
@@ -0,0 +1,10 @@
+# Get environment variable fom command line
+
+import os
+
+api_token = (os.getenv("api_token"))
+
+# check using print(os.getenv("api_token"))
+
+#In command line export api_token="123456"
+
